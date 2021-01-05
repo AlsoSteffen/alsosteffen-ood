6272a5e2536ad94df547ec521393295c3d7231dc
AlsoSteffen <52997388+AlsoSteffen@users.noreply.github.com>
2021-01-05 17:30:05 +0100

    Refactored MenuController.java 1. moved menu generation from constructor to methods 2. commented out AboutBox.java (temporary before deletion later) 3. moved contents from AboutBox.java to MenuController.createAbout

--- src/MenuController.java
+++ src/MenuController.java
@@ -1,13 +1,8 @@
-import java.awt.MenuBar;
-import java.awt.Frame;
-import java.awt.Menu;
-import java.awt.MenuItem;
-import java.awt.MenuShortcut;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
-
-import javax.swing.JOptionPane;
 
 /**
  * <p>The controller for the menu</p>
@@ -39,13 +34,42 @@ public class MenuController extends MenuBar
     private final Frame parent; //The frame, only used as parent for the Dialogs
     private final Presentation presentation; //Commands are given to the presentation
 
+
     public MenuController(Frame frame, Presentation pres)
     {
         parent = frame;
         presentation = pres;
-        MenuItem menuItem;
+
         Menu fileMenu = new Menu(FILE);
-        fileMenu.add(menuItem = mkMenuItem(OPEN));
+        add(fileMenu);
+        Menu viewMenu = new Menu(VIEW);
+        add(viewMenu);
+        Menu helpMenu = new Menu(HELP);
+        add(helpMenu);
+
+        createOpenPresentation(fileMenu, presentation);
+        createNewPresentation(fileMenu, presentation);
+        createSavePresentation(fileMenu, presentation);
+        createExitPresentation(fileMenu, presentation);
+        createNextSlide(viewMenu, presentation);
+        createPreviousSlide(viewMenu, presentation);
+        createGoTo(viewMenu, presentation);
+        createAbout(helpMenu, presentation);
+
+    }
+
+    //Creating a menu-item
+    public MenuItem mkMenuItem(String name)
+    {
+        return new MenuItem(name, new MenuShortcut(name.charAt(0)));
+    }
+
+    private void createOpenPresentation(Menu menu, Presentation presentation)
+    {
+        MenuItem menuItem;
+
+        menu.add(menuItem = mkMenuItem(OPEN));
+
         menuItem.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent actionEvent)
@@ -60,13 +84,18 @@ public class MenuController extends MenuBar
                 catch (IOException exc)
                 {
                     JOptionPane.showMessageDialog(parent, IOEX + exc,
-                                                  LOADERR, JOptionPane.ERROR_MESSAGE
-                    );
+                                                  LOADERR, JOptionPane.ERROR_MESSAGE);
                 }
                 parent.repaint();
             }
         });
-        fileMenu.add(menuItem = mkMenuItem(NEW));
+    }
+
+    private void createNewPresentation(Menu menu, Presentation presentation)
+    {
+        MenuItem menuItem;
+
+        menu.add(menuItem = mkMenuItem(NEW));
         menuItem.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent actionEvent)
@@ -75,7 +104,13 @@ public class MenuController extends MenuBar
                 parent.repaint();
             }
         });
-        fileMenu.add(menuItem = mkMenuItem(SAVE));
+    }
+
+    private void createSavePresentation(Menu menu, Presentation presentation)
+    {
+        MenuItem menuItem;
+
+        menu.add(menuItem = mkMenuItem(SAVE));
         menuItem.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent e)
@@ -89,12 +124,18 @@ public class MenuController extends MenuBar
                 {
                     JOptionPane.showMessageDialog(parent, IOEX + exc,
                                                   SAVEERR, JOptionPane.ERROR_MESSAGE
-                    );
+                                                 );
                 }
             }
         });
-        fileMenu.addSeparator();
-        fileMenu.add(menuItem = mkMenuItem(EXIT));
+    }
+
+    private void createExitPresentation(Menu menu, Presentation presentation)
+    {
+        MenuItem menuItem;
+
+        menu.addSeparator();
+        menu.add(menuItem = mkMenuItem(EXIT));
         menuItem.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent actionEvent)
@@ -102,9 +143,13 @@ public class MenuController extends MenuBar
                 presentation.exit(0);
             }
         });
-        add(fileMenu);
-        Menu viewMenu = new Menu(VIEW);
-        viewMenu.add(menuItem = mkMenuItem(NEXT));
+    }
+
+    private void createNextSlide(Menu menu, Presentation presentation)
+    {
+        MenuItem menuItem;
+
+        menu.add(menuItem = mkMenuItem(NEXT));
         menuItem.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent actionEvent)
@@ -112,7 +157,13 @@ public class MenuController extends MenuBar
                 presentation.nextSlide();
             }
         });
-        viewMenu.add(menuItem = mkMenuItem(PREV));
+    }
+
+    private void createPreviousSlide(Menu menu, Presentation presentation)
+    {
+        MenuItem menuItem;
+
+        menu.add(menuItem = mkMenuItem(PREV));
         menuItem.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent actionEvent)
@@ -120,7 +171,13 @@ public class MenuController extends MenuBar
                 presentation.prevSlide();
             }
         });
-        viewMenu.add(menuItem = mkMenuItem(GOTO));
+    }
+
+    private void createGoTo(Menu menu, Presentation presentation)
+    {
+        MenuItem menuItem;
+
+        menu.add(menuItem = mkMenuItem(GOTO));
         menuItem.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent actionEvent)
@@ -130,22 +187,31 @@ public class MenuController extends MenuBar
                 presentation.setSlideNumber(pageNumber - 1);
             }
         });
-        add(viewMenu);
-        Menu helpMenu = new Menu(HELP);
-        helpMenu.add(menuItem = mkMenuItem(ABOUT));
+    }
+
+    private void createAbout(Menu menu, Presentation presentation)
+    {
+        MenuItem menuItem;
+
+        menu.add(menuItem = mkMenuItem(ABOUT));
         menuItem.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent actionEvent)
             {
-                AboutBox.show(parent);
+                String message = "JabberPoint is a primitive slide-show program in Java(tm). It\n" +
+                        "is freely copyable as long as you keep this notice and\n" +
+                        "the splash screen intact.\n" +
+                        "Copyright (c) 1995-1997 by Ian F. Darwin, ian@darwinsys.com.\n" +
+                        "Adapted by Gert Florijn (version 1.1) and " +
+                        "Sylvia Stuurman (version 1.2 and higher) for the Open" +
+                        "University of the Netherlands, 2002 -- now.\n" +
+                        "Author's version available from http://www.darwinsys.com/";
+
+                String title = "About JabberPoint";
+
+                JOptionPane.showMessageDialog(parent, message, title, JOptionPane.INFORMATION_MESSAGE);
             }
         });
-        setHelpMenu(helpMenu);        //Needed for portability (Motif, etc.).
     }
 
-    //Creating a menu-item
-    public MenuItem mkMenuItem(String name)
-    {
-        return new MenuItem(name, new MenuShortcut(name.charAt(0)));
-    }
 }
